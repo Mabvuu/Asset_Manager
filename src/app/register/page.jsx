62c3972efb8596febe08e1ca24dfc9c3a848a2d8
Mabvuu <pattymabvuu@gmail.com>
2025-12-03 03:49:34 +0200

    remove thirdweb and ethers - fix build

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,7 @@
 // src/app/register/page.jsx
 'use client';
 import { useState } from 'react';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../..//lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 
 export default function RegisterPage() {
